7ec3f397986745b0befda97e40d5b548cab0fb60
Umut Yilmaz <224594783+Hummus404@users.noreply.github.com>
2026-06-08 11:06:54 +0100

    Initialize playwright

---
+++ src/test/java/com/makersacademy/acebook/feature/SignUpTest.java
@@ -0,0 +1,49 @@
+package com.makersacademy.acebook.feature;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+import com.github.javafaker.Faker;
+import com.microsoft.playwright.options.AriaRole;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.microsoft.playwright.*;
+
+public class SignUpTest {
+
+    Playwright playwright;
+    Browser browser;
+    Page page;
+    Faker faker;
+
+    @BeforeEach
+    public void setup() {
+        faker = new Faker();
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+        page = browser.newPage();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    public void successfulSignUpAlsoLogsInUser() {
+        String email = faker.name().username() + "@email.com";
+
+        page.navigate("http://localhost:8081/");
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Sign up")).click();
+        page.locator("[name=email]").fill(email);
+        page.locator("[name=password]").fill("P@55qw0rd");
+        page.locator("[name=action]").click();
+        page.locator("[name=action]").filter(new Locator.FilterOptions().setHasText("Accept")).click();
+        Locator greeting = page.locator("#greeting");
+
+        assertThat(greeting).hasText("Signed in as " + email);
+    }
+}
