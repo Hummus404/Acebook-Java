aacc90fde933dff1e01dc7eb7d7bf04ffaa2f886
mpmua <mgr_55@hotmail.com>
2026-06-09 16:21:57 +0100

    wip: add logic which allows image location to be saved to the database

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -6,9 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class PostsController {
@@ -25,8 +32,29 @@ public class PostsController {
     }
 
     @PostMapping("/posts")
-    public RedirectView create(@ModelAttribute Post post) {
+    public RedirectView create(@ModelAttribute Post post, @RequestParam("image") MultipartFile image) throws IOException {
+
+        // Could implement a try catch block at a later point
+
+        Path uploadDir = Paths.get("images");
+        Files.createDirectories(uploadDir);
+
+        String filename = System.currentTimeMillis() + "_" + image.getOriginalFilename();
+
+        Path filePath = uploadDir.resolve(filename);
+
+        Files.copy(
+                image.getInputStream(),
+                filePath,
+                StandardCopyOption.REPLACE_EXISTING
+        );
+
         repository.save(post);
+
+        post.setImage(filePath.toString());
+        System.out.println("HELLO");
+        System.out.println(filePath.toString());
+
         return new RedirectView("/posts");
     }
 }
