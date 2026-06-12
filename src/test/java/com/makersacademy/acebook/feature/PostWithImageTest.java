9572ab2c0e6cab4f1c468ffef6f63d5c27d9c095
mpmua <mgr_55@hotmail.com>
2026-06-12 12:50:10 +0100

    fix: improve test stability with improved waits

--- src/test/java/com/makersacademy/acebook/feature/PostWithImageTest.java
+++ src/test/java/com/makersacademy/acebook/feature/PostWithImageTest.java
@@ -32,10 +32,11 @@ public class PostWithImageTest {
         driver = new ChromeDriver();
         faker = new Faker();
 
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         driver.get("http://localhost:8081/");
         wait.until(driver -> driver.findElement(By.name("username")));
+        wait.until(driver -> driver.findElement(By.name("password")));
         driver.findElement(By.name("username")).sendKeys("test1234@email.com");
         driver.findElement(By.name("password")).sendKeys(" -6G3b5!(dOcJ[c')^&>8M5(w");
         driver.findElement(By.name("action")).click();
@@ -52,7 +53,8 @@ public class PostWithImageTest {
     @Test
     public void successfulImageUpload() {
 
-        int before = wait.until(driver -> driver.findElements(By.cssSelector("li img")).size());
+        wait.until(driver -> driver.findElements(By.cssSelector("li img")));
+        int before = driver.findElements(By.cssSelector("li img")).size();
 
         Path imagePath = Paths.get(
                 System.getProperty("user.dir"),
@@ -76,10 +78,11 @@ public class PostWithImageTest {
     public void successfulContentUpload() throws InterruptedException {
 
         String dummyInputText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel lorem cursus quam gravida sagittis. Cras ac placerat erat. Donec efficitur venenatis augue in fermentum. Cras finibus condimentum nunc, in ornare elit malesuada eu. Fusce sagittis mollis justo eget vehicula. Nullam tristique luctus sapien nec bibendum. Vivamus pretium mauris eget.";
+        wait.until(driver -> driver.findElement(By.className("input-content-field")));
         driver.findElement(By.className("input-content-field")).sendKeys(dummyInputText);
-        TimeUnit.SECONDS.sleep(5);
         driver.findElement(By.className("submit-btn")).click();
-        TimeUnit.SECONDS.sleep(5);
+        wait.until(driver -> driver.findElement(By.className("input-content-field")));
+//        wait.until(driver -> driver.findElement(By.tagName("body")));
         String pageText = driver.findElement(By.tagName("body")).getText();
         assertTrue(pageText.contains(dummyInputText));
 
@@ -99,12 +102,17 @@ public class PostWithImageTest {
                 "test-image.jpg"
         );
 
+        wait.until(driver -> driver.findElement(By.name("imageFile")));
+
         driver.findElement(By.name("imageFile"))
                 .sendKeys(imagePath.toString());
         driver.findElement(By.className("input-content-field")).sendKeys(dummyInputText);
         int before = driver.findElements(By.cssSelector("li img")).size();
         driver.findElement(By.className("submit-btn")).click();
+
+        wait.until(driver -> driver.findElement(By.cssSelector("li img")));
         int after = driver.findElements(By.cssSelector("li img")).size();
+        wait.until(driver -> driver.findElement(By.className("input-content-field")));
         String pageText = driver.findElement(By.tagName("body")).getText();
         assertTrue(pageText.contains(dummyInputText));
         assertTrue(after > before);
@@ -114,11 +122,7 @@ public class PostWithImageTest {
 
     @Test
     public void emptyPostIsNotSubmitted() {
-        System.out.println(System.getProperty("user.dir"));
-        WebElement submitBtn = wait.until(
-                ExpectedConditions.elementToBeClickable(By.className("submit-btn"))
-        );
-        submitBtn.click();
+        wait.until(driver -> driver.findElement(By.className("submit-btn"))).click();
         Alert alert = wait.until(ExpectedConditions.alertIsPresent());
         assertEquals("Both image and content fields cannot be empty", alert.getText());
         alert.accept();
