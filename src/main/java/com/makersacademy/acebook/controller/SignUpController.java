6b330f75e6a4909975e87ccc7919fd44156eecf3
mpmua <mgr_55@hotmail.com>
2026-06-17 15:40:41 +0100

    fix: resolve issue with /posts page not loading after sign up

--- src/main/java/com/makersacademy/acebook/controller/SignUpController.java
+++ src/main/java/com/makersacademy/acebook/controller/SignUpController.java
@@ -198,15 +198,14 @@ public class SignUpController {
 
         user.setEmailAddress(principal.getEmail());
 
-        Optional<User> newUser = userRepository.findUserByEmailAddress(user.getEmailAddress());
 
         userRepository.save(user);
+
+        Optional<User> newUser = userRepository.findUserByEmailAddress(user.getEmailAddress());
 
         session.setAttribute("profilePicture", newUser.get().getProfilePicture());
         session.setAttribute("userID", newUser.get().getId());
         session.setAttribute("userUsername", newUser.get().getUsername());
-
-
 
         return new RedirectView("/posts");
     }
